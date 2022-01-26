b273270422dfdeb4e434c03fb766d8a23d56af24
TimoSchmidtt <timos.minilaptop@gmail.com>
2022-01-26 11:18:52 +0100

    F1.2.02.L2

---
+++ feestlunch.py
@@ -0,0 +1,9 @@
+CroissantPrijs = 0.39
+StokbroodPrijs = 2.78
+KortingBon = -0.5
+Totaal = 0.0
+AantalCroissant = 17
+AantalStokbrood = 2
+AantalKortingBon = 3
+
+print((AantalCroissant * CroissantPrijs) + (AantalStokbrood * StokbroodPrijs) + (AantalKortingBon * KortingBon))
---
+++ speelhaldag.py
@@ -0,0 +1,8 @@
+Personen = 4.0
+ToegangsTicketPrijs = 7.45
+GameSeatPrijs = 0.37
+GameSeatMinutenPerPrijs = 5.0
+MinutenInGameSeat = 45.0
+
+print(Personen*(ToegangsTicketPrijs+(GameSeatPrijs*(MinutenInGameSeat/GameSeatMinutenPerPrijs))))
+
